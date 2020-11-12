7d58f4feb4d2b189d8c6a354023994586a1a2d04
Ben Everard <ben@theideabureau.co>
2020-11-12 12:21:55 +0000

    Added handle membership trigger

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
-// if the donate handle function exists, fire it
+// if the donate handler function exists, fire it
 if (typeof handleDonatePage === "function") { 
     handleDonatePage(jQuery);
 }
 
+// if the membership handler function exists, fire it
+if (typeof handleMembershipPage === "function") { 
+    handleMembershipPage(jQuery);
+}
 
 // if a URL hash exists, attempt to click the corresponding element
 jQuery(document).ready(function($) {
